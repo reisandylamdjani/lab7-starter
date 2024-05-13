388380a8b32e6e11d0bd807836f49c89b6992660
reisandylamdjani <lamdjani@gmail.com>
2024-05-12 23:47:37 -0700

    started lab 7

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -12,6 +12,27 @@ class RecipeCard extends HTMLElement {
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything in the <style> tag>)
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+
+		let shadowElement = this.attachShadow({mode:'open'});
+		let articleRoot = document.createElement('article');
+		let styleRoot = document.createElement('style');
+
+		let starEl = document.createElement('*');
+		let aEl = document.createElement('a');
+		let divEl = document.createElement('div');
+		let pEl = document.createElement('p');
+		let spanEl = document.createElement('span');
+		let timeEl = document.createElement('time');
+
+		styleRoot.append(starEl);
+		styleRoot.append(aEl);
+		styleRoot.append(divEl);
+		styleRoot.append(pEl);
+		styleRoot.append(spanEl);
+		styleRoot.append(timeEl);
+
+		shadowElement.append(articleRoot);
+		shadowElement.append(styleRoot);
 	}
 
 	/**
@@ -46,8 +67,10 @@ class RecipeCard extends HTMLElement {
 		//           literals (template strings) and element.innerHTML for this.
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
+		
 	}
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
